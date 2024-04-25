292c181865e594e551afe8102779be014575236a
Milena Piovesam <milepiovesan2@gmail.com>
2024-04-25 08:15:32 -0300

    lista de exercicos estrutura de dados

---
+++ estrutura_de_dados.py
@@ -0,0 +1,34 @@
+# Exercício 1
+
+# gastos = [2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08]
+
+# media_gastos = sum(gastos)/len(gastos)
+# print(media_gastos)
+
+# Exercício 2
+
+# gastos = [2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08]
+
+# gastos_acima_3000 = []
+
+# for gasto in gastos:
+#     if gasto > 3000:
+#         gastos_acima_3000.append(gasto)
+
+# soma_gastos_acima_3000 = len(gastos_acima_3000)
+# print(soma_gastos_acima_3000)
+
+#Exercício 3 e 4 
+# lista = []
+
+# for i in range(1, 6):
+#     try:
+#         numero = int(input("Digite um número:"))
+#         lista.append(numero)
+#     except:
+#         print ("Caractere invlaído.")
+
+# lista_inversa = lista.reverse() #Para imprimir uma lista na ordem inversa a que foi enviada.1
+
+# print(lista)
+
